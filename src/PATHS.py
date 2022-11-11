cb6bc6d74ed61573a9c63b9910ce157f23ae7830
juan <juankuntz@protonmail.com>
2022-11-11 17:56:46 +0000

    Fixed paths.py

--- src/PATHS.py
+++ src/PATHS.py
@@ -1,7 +1,5 @@
 from os.path import dirname, realpath
 
 REPOSITORY = dirname(dirname(realpath(__file__)))
-EQUIPMENT_INFORMATION = REPOSITORY + '/db/equipment_information.xlsx'
-UPCOMING_SERVICES = REPOSITORY + '/queries/upcoming_services.xlsx'
-SERVICES_IN_RANGE = REPOSITORY + '/queries/services_in_range.xlsx'
-QUERIES = REPOSITORY + '/queries/queries.xlsx'
+EQUIPMENT_INFORMATION = REPOSITORY + '/equipment_information.xlsx'
+QUERIES = REPOSITORY + '/queries.xlsx'
